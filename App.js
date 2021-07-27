f08d2bb798c4fd678624e4af7f5d778a730365ed
ShoaibTamboli <shoaibtamboli03@gmail.com>
2021-07-27 23:47:18 +0530

    Update: Input Output

--- App.js
+++ App.js
@@ -1,33 +1,11 @@
-// console.log('Hello');
+var Inputtext = document.querySelector('#textarea');
+var Outputtext=document.querySelector('#output');
+var Transaltebutton =document.querySelector('.btn-translate')
 
-// console.log("This is test")
+function clickhandler() {
+    Outputtext.innerHTML="fiojsdfhisdhiofh "+ Inputtext.value;
+    
+};
 
-// prompt("Enter user name");
-// alert("This isalert")
+Transaltebutton.addEventListener("click", clickhandler)
 
-
-// var username=prompt("Enter usernmame");
-// alert("you entered"+username)
-
-// console.log("you are welcome",username)
-
-
-// ////
-// //input
-// var un =prompt("Enter day")
-// //processing
-// var unm="you have entered "+ un;
-// //output
-
-// alert(unm)
-
-var translateButton=document.querySelector("#btn-translate");
-var textinput = document.querySelector('#textarea')
-//console.log(translateButton)
-
-translateButton.addEventListener("click",function clickEventhandler() {
-    console.log("Clicked!")
-    console.log("Input entered:", textinput.value);
-})
-
-//console.log(textinput)
---
+++ Appdemo.js
@@ -0,0 +1,38 @@
+// console.log('Hello');
+
+// console.log("This is test")
+
+// prompt("Enter user name");
+// alert("This isalert")
+
+
+// var username=prompt("Enter usernmame");
+// alert("you entered"+username)
+
+// console.log("you are welcome",username)
+
+
+// ////
+// //input
+// var un =prompt("Enter day")
+// //processing
+// var unm="you have entered "+ un;
+// //output
+
+// alert(unm)
+
+var translateButton=document.querySelector("#btn-translate");
+var textinput = document.querySelector('#textarea')
+//console.log(translateButton)
+
+translateButton.addEventListener("click",function clickEventhandler() {
+    console.log("Clicked!")
+    console.log("Input entered:", textinput.value);
+})
+
+//console.log(textinput)
+
+// 1. document.querySelector(textarea)
+// 2. .btn-primary
+// 3. #btn-translate
+// 4. "input[name='translator']"
