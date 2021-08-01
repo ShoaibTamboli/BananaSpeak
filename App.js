6e8834138e1c07aa0619cfcf99bc5b439c0b0bcc
ShoaibTamboli <shoaibtamboli03@gmail.com>
2021-08-01 21:56:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ var Transaltebutton = document.querySelector('.btn-translate')
 //var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 //var serverURL ="https://api.funtranslations.com/translate/pirate.json"
+
 function getTranslateURL(text) {
     return serverURL + "?" + "text=" + text;
 }
